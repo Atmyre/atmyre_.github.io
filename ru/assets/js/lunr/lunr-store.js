e4abd3e5234093758da3e6274f9c9b6ec855cd54
Atmyre <Atmyre@users.noreply.github.com>
2023-03-20 19:52:59 +0000

    jekyll build from Action 9d26c465bca0c5bf19b2c98db84f545e2f47d7b3

---
+++ ru/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Добро пожаловать!",
+        "excerpt":"Первый пост  ","categories": ["blog"],
+        "tags": ["post","update"],
+        "url": "/ru/blog/first/",
+        "teaser": null
+      },]
---
+++ ru/build/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome!",
+        "excerpt":"The first post  ","categories": ["blog"],
+        "tags": ["post","update"],
+        "url": "/blog/first/",
+        "teaser": null
+      },]
